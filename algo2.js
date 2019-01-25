b17ea2317e10b719a29917255423f33132a6e3b7
JeanVernus <jeannywcs@gmail.com>
2019-01-25 14:19:32 +0100

    exercice algo2

--- algo2.js
+++ algo2.js
@@ -17,18 +17,22 @@
 * Résultat : [7, 85]
 */
 
-var minMax = () => (array) => {
-  const min = 0;
-  const max = array[0];
+const minMax = (array) => {
 
-  for (let i = 1; i = array.length - 1; i+1) {
-    if (array(i) < min) {
-      min = array;
+
+  var min = 100;
+  var max = -100;
+  var retourne = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
     }
-    if (array[i] = max) {
+    if (array[i] > max) {
       max = array[i];
     }
-    return array;
-  }
-  array.push(min, max);
+ }
+  retourne.push(min, max);
+  return retourne ;
 }
+console.log(minMax());
