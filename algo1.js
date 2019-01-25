43a8716569356003009a78447be62f26dc48340a
Bourlon lyann <bourlon.lyann84@gmail.com>
2019-01-25 11:08:49 +0100

    Ajout de l'algo 1 terminé

--- algo1.js
+++ algo1.js
@@ -17,3 +17,15 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+function idSieges() {
+  let resultat = []
+  for (let i = 1; i < 27; i++ ) {
+    for (let j = 1; j < 101; j++) {
+      resultat.push(i + '-' + j);
+    }
+  }
+  return resultat;
+
+};
+ 
